a4bc7a5db8cf8cb359e898c80641d69808e01da1
Mazuba Mudenda <mazmudenda404@gmail.com>
2021-09-23 10:56:17 +0200

    Toll app for advance payment of toll fees

--- app/src/main/java/com/example/etoll_system/User/MainActivity.java
+++ app/src/main/java/com/example/etoll_system/User/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.etoll_system.User;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.etoll_system.R;
+import com.example.etoll_system.common.RegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    Button loginBtn;
+    Button createBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+
+        loginBtn=findViewById(R.id.loginBtn);
+        createBtn=findViewById(R.id.create_account_btn);
+
+
+        loginBtn.setOnClickListener(view -> {
+            Intent intent =new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        createBtn.setOnClickListener(view -> {
+            Intent intent =new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/etoll_system/common/LoginActivity.java
+++ app/src/main/java/com/example/etoll_system/common/LoginActivity.java
@@ -1,65 +1,30 @@
 package com.example.etoll_system.common;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Pair;
-import android.view.View;
-import android.view.WindowManager;
-
 import com.example.etoll_system.R;
-import com.example.etoll_system.User.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login2);
-    }
+        setContentView(R.layout.activity_login);
 
-
-
-    public void callRegisterScreen(View view){
-        Intent intent = new Intent(getApplicationContext(),RegistrationActivity.class);
-
-        Pair[] pairs = new Pair[1];
-
-        pairs[0] = new Pair<View,String>(findViewById(R.id.create_account_btn),"transition_create");
-
-        if (android.os.Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.LOLLIPOP) {
-            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
-
-            startActivity(intent,options.toBundle());
-        }
-        else{
-            startActivity(intent);
-        }
 
     }
 
 
 
-    public void callMainScreen(View view){
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
-        Pair[] pairs = new Pair[1];
 
-        pairs[0] = new Pair<View,String>(findViewById(R.id.create_account_btn),"transition_create");
 
-        if (android.os.Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.LOLLIPOP) {
-            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
 
-            startActivity(intent,options.toBundle());
-        }
-        else{
-            startActivity(intent);
-        }
-
-    }
 }
